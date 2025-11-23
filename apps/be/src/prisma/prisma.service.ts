3f4dccc6a41957eb3048b7b51dd29f0c8df86f0c
H.W <olt13mm@gmail.com>
2025-11-23 17:36:49 +0900

    feat: add query logging to PrismaService

--- apps/be/src/prisma/prisma.service.ts
+++ apps/be/src/prisma/prisma.service.ts
@@ -6,5 +6,11 @@ import { PrismaClient } from "@repo/database/client";
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+
+    // Log all queries to the console
+    this.$on('query', (e) => {
+      console.log('Query:', e.query);
+    });  
+
   }
 }
