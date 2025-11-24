128b0811b4a68460213f91909ea8737ccb713d0d
H.W <olt13mm@gmail.com>
2025-11-24 23:18:56 +0900

    feat: api requests 방식 axios 적용

--- apps/fe/src/api/links/index.ts
+++ apps/fe/src/api/links/index.ts
@@ -1,28 +1,15 @@
 import type { Link } from '@repo/api';
-
-const API_URL = process.env.API_URL || 'http://localhost:3000';
-
-// export const dynamic = 'force-dynamic';
-
-export const Fetch = async (
-  input: string | URL | Request,
-  init?: RequestInit,
-): Promise<Response> => {
-  return fetch(`${API_URL}${input}`, init);
-};
+import { api } from '@config/axios';
 
 export async function getLinks(): Promise<Link[]> {
   try {
-    const res = await Fetch(`/links`, {
-      // cache: 'no-store',
-      // next: { revalidate: 0 },
-    });
+    const res = await api.get<Link[]>(`/links`);
 
-    if (!res.ok) {
+    if (!res.data) {
       throw new Error('Failed to fetch links');
     }
 
-    return res.json();
+    return res.data;
   } catch (error) {
     console.error('Error fetching links:', error);
     return [];
---
+++ apps/fe/src/config/axios.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosInstance, AxiosError } from 'axios';
+
+interface IError {
+  code: number;
+  error: string;
+  message: string;
+}
+
+export type TAxiosError = AxiosError<IError>;
+
+const baseURL =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:3000'
+    : process.env.NEXT_PUBLIC_SERVER_URL;
+
+export const api: AxiosInstance = axios.create({
+  baseURL,
+  withCredentials: false,
+});
