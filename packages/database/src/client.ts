df02213266eae00254c1e8f0268e9d280c7aa636
Hong Wook Choo <hw.choo@amicuslex.net>
2025-11-24 16:10:18 +0900

    fix: globalForPrisma 타입 정의 수정

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '../generated/client/index.js';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as typeof globalThis & { prisma?: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
